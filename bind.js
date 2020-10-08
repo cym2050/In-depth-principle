3e932a1d4f83c9d95e6d8fc8570a959ad722631f
cym2050 <1390062269@qq.com>
2020-10-08 20:15:46 +0800

    手写 bind

---
+++ bind.js
@@ -0,0 +1,23 @@
+// 手写 Bind
+
+Function.prototype.myBind = function() {
+	// 将参数拆解为数组
+	const args = Array.prototype.slice.call(arguments)
+	// 获取 this
+	const t = args.shift()
+	// fn.bind(...) 中的 fn
+	const self = this
+	// 返回一个函数
+	return function() {
+		return self.apply(t, args)  
+	}
+}
+
+function fn1(a, b, c) {
+	console.log('this', this)
+	console.log(a, b, c)
+	return 'this is fn1'
+}
+
+const fn2 = fn1.myBind({ name: 'xialuo'}, 10, 20, 30)
+fn2()
